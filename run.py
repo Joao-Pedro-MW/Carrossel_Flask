4961c3893e76eabbc71a2a1f99d85bcef3446115
João Pedro Müller Wilges <80231608+Joao-Pedro-MW@users.noreply.github.com>
2022-04-23 23:57:37 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,9 @@
+from app import app
+if __name__ == "__main__":
+    app.run()
+    
+#definimos nosso programa para funcionar neste arquivo
+#comandos para funcionar
+#set FLASK_APP = run.py
+#set FLASK_ENV = production ou  development
+#flask run --host=0.0.0.0 para rodar localmente em uma LAN
